3d9f6c8bf0ee48f2c6a3dc4831e5d527ddf5cf84
Alessandro Terracciano <terracciano41@gmail.com>
2022-02-09 18:16:22 +0100

    es completato

---
+++ main.js
@@ -0,0 +1,10 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+var app = new Vue(
+    {
+        el: "#root",
+        data: {
+            message: "Questo titolo è stato generato con Vue.js!"
+        }
+    }
+);
